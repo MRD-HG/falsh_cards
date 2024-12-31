b0389690a88d1ff62246fdcf7bae2b4cf71070e5
MRD-HG <roudani.mustapha0@gmail.com>
2024-12-31 13:17:31 +0100

    Adding

--- lib/View/FlashCard.dart
+++ lib/View/FlashCard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:falsh_cards/quote.dart';
 class FlashCard extends StatefulWidget {
   const FlashCard({super.key});
 
@@ -7,37 +8,25 @@ class FlashCard extends StatefulWidget {
 }
 
 class _FlashCardState extends State<FlashCard> {
+  List<Quote> quotes=[
+    Quote(text: 'The greatest glory in living lies not in never falling, but in rising every time we fall.', author: 'Nelson Mandela'),
+    Quote(text: 'The way to get started is to quit talking and begin doing', author: 'Walt Disney'),
+    Quote(text: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking", author: 'Steve Jobs'),
+
+  ];
+
+
   @override
   Widget build(BuildContext context) {
-    return  Row(
+    return  Scaffold(
+      body:Column(
+        children:[
+          Card(
+            child: ListTile(),
+          )
+        ]
 
-      children:[
-        Expanded(
-          flex: 2,
-          child: Container(
-            padding: const EdgeInsets.all(30),
-            color: Colors.blueAccent,
-            child:const Text("1"),
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            padding: const EdgeInsets.all(30),
-            color: Colors.grey,
-            child:const Text("2"),
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            padding: const EdgeInsets.all(30),
-            color: Colors.orange,
-            child:const Text("3"),
-          ),
-        ),
-
-      ],
+      ) ,
     );
   }
 }
